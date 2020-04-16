d6395dff93dd2f05f622303f7c15a3c5926cbaaa
BboyFane <hua.stephane@gmail.com>
2020-04-16 13:10:40 +0200

    Initialization to a blank RN app

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      {/* {content} */}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+});
+
+// export default App;
