927746f5e934d0b86f4c0c4b316555d70b470858
BboyFane <hua.stephane@gmail.com>
2020-05-29 00:53:00 +0200

    Push skeleton with code

---
+++ screens/HomeShare.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Card from '../components/Card';
+
+const HomeShare = props => {
+    return (
+        <View></View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default HomeShare;
