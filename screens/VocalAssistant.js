c12f81e903c860c7f8d490238c0a9dfe523fa72e
BboyFane <hua.stephane@gmail.com>
2020-08-30 18:03:51 +0200

    Add recorder logic

--- screens/VocalAssistant.js
+++ screens/VocalAssistant.js
@@ -2,18 +2,36 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Fab from '../components/Fab';
 import Icons from 'react-native-vector-icons/MaterialIcons';
+import AudioRecord from 'react-native-audio-record';
 import io from 'socket.io-client';
 const socket = io.connect('https://familyhome.herokuapp.com/')
 
 const VocalAssistant = () => {
     const [isListening, setIsListening] = useState(false)
     const startStop = () => {
-        isListening == true ? setIsListening(false) : (
-            listening()
-        )
+        isListening == false ? listening() : stopListening()
     }
-    const listening = () => {
+    const listening = async () => {
         setIsListening(true)
+        const options = {
+            sampleRate: 16000,  // default 44100
+            channels: 1,        // 1 or 2, default 1
+            bitsPerSample: 16,  // 8 or 16, default 16
+            audioSource: 6      // android only (see below)
+        };
+        console.log('START');
+        AudioRecord.init(options);
+        AudioRecord.start();
+        // or to get the wav file path
+        audioFile = await AudioRecord.stop();
+        AudioRecord.on('data', data => {
+            console.log('Record', data);
+        });
+    }
+    const stopListening = () => {
+        AudioRecord.stop()
+        console.log('STOP');
+        setIsListening(false)
     }
 
     socket.emit('Created');
@@ -21,7 +39,7 @@ const VocalAssistant = () => {
         console.log('data: ', data);
         socket.close();
     })
-    
+
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Votre assistant vocal</Text>
